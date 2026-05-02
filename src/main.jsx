1f6775e8d3ca616fffef6df7c0229056b7b2b6b9
PrashantK131 <rahulprashant2001@gmail.com>
2026-05-02 15:13:22 +0530

    Added Provider to the main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Provider } from 'react-redux';
+import store from './store/index';
 import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Redux Provider wraps the entire app */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
